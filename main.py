9dfb3268fe893ce2ecfde843d423c7ee606380f9
adam.nouari <adamnouari93@gmail.com>
2024-09-30 12:01:08 +0000

    ASCII fait

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""
+Module qui construit une liste de tuples, correspondant au codage d'une chaîne de caractères
+"""
+
 #### Imports et définition des variables globales
 
 
@@ -5,7 +9,8 @@
 
 
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+    """
+    Retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -13,14 +18,25 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
-
-    return [ ]
+    C = [s[0]]
+    O = [1]
+    n = len(s)
+    I = []
+    compteur = 1
+    for k in range(1,n) :
+        if s[k] == s[k-1] :
+            O[-1]+=1
+        else :
+            I.append((C[-1],O[-1]))
+            C.append(s[k])
+            O.append(1)
+    I.append((C[-1],O[-1]))
+    return I
 
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+    """
+    retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -28,14 +44,15 @@ def artcode_r(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
-
-    # cas de base
-    # recherche nombre de caractères identiques au premier
-    # appel récursif
-
-    return []
+    if not s:
+        return []
+    lettre1 = s[0]
+    compteur = 0
+    n = len(s)
+    while compteur < n and s[0] == lettre1 :# compte le nbre d'occurences du premier caractère
+        compteur+=1
+    I = [(lettre1,compteur)]
+    return I + artcode_r(s[compteur:])
     
 
 #### Fonction principale
@@ -47,3 +64,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
